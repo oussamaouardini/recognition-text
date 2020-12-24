dab2bd05b8368a83f90c83b2fd1f2a4ed9be3fea
oussamaouardnini <ouss.ouardini@gmail.com>
2020-12-24 23:09:15 +0100

    printing Scan Result as Pdf

--- lib/screens/scanedFiles_screen.dart
+++ lib/screens/scanedFiles_screen.dart
@@ -1,11 +1,17 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share/share.dart';
+import 'package:sweetalert/sweetalert.dart';
 import 'package:text_recognition_app/utilities/size_config.dart';
 import 'package:text_recognition_app/utilities/styles.dart';
-import 'package:flutter/services.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 // ignore: must_be_immutable
 class ScannedFilesScreen extends StatefulWidget {
@@ -19,9 +25,6 @@ class ScannedFilesScreen extends StatefulWidget {
 
 class _ScannedFilesScreenState extends State<ScannedFilesScreen>
     with SingleTickerProviderStateMixin {
-
-
-
   /// Map de give IconData to the drop down item icon
   Map<String, IconData> iconDropDown = {
     'Save': Icons.save,
@@ -29,6 +32,7 @@ class _ScannedFilesScreenState extends State<ScannedFilesScreen>
   };
   String dropdownValue = 'Save';
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -85,8 +89,8 @@ class _ScannedFilesScreenState extends State<ScannedFilesScreen>
                 elevation: 0,
                 dropdownColor: Colors.white,
                 underline: SizedBox(),
-                style: TextStyle(color: Colors.grey,
-                    fontWeight: FontWeight.bold),
+                style:
+                    TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
                 onChanged: (String newValue) async {
                   setState(() {
                     dropdownValue = newValue;
@@ -105,7 +109,7 @@ class _ScannedFilesScreenState extends State<ScannedFilesScreen>
                           color: Colors.grey,
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(left:4.0),
+                          padding: const EdgeInsets.only(left: 4.0),
                           child: Text(value),
                         ),
                       ],
@@ -169,19 +173,45 @@ class _ScannedFilesScreenState extends State<ScannedFilesScreen>
                 backgroundColor: Colors.red,
                 label: 'PDF',
                 labelStyle: TextStyle(fontSize: 18.0),
-                onTap: () => print('FIRST CHILD')),
+                onTap: () async {
+                  try {
+                    final pdf = pw.Document();
+                    pdf.addPage(pw.Page(
+                        pageFormat: PdfPageFormat.a4,
+                        build: (pw.Context context) {
+                          return pw.Center(
+                            // Todo: add a font to the pdf
+                            child: pw.Text("Hello World",
+                                style: pw.TextStyle(
+                                    font: pw.Font.helvetica(),
+                                    color: PdfColor.fromInt(22))),
+                          ); // Center
+                        }));
+                    final output = await getTemporaryDirectory();
+                    var fileNme = "${DateTime.now().toString()}.pdf";
+                    final file = File("${output.path}/$fileNme");
+                    await file.writeAsBytes(pdf.save());
+                    await Printing.sharePdf(
+                        bytes: pdf.save(), filename: '$fileNme');
+                  } catch (Exception) {
+                    print(Exception);
+                    SweetAlert.show(context,
+                        title: "Opps",
+                        subtitle: "An error occured please try again!!!",
+                        style: SweetAlertStyle.error);
+                  }
+                }),
             SpeedDialChild(
               child: Icon(Icons.share),
               backgroundColor: Colors.green,
               label: 'Share',
               labelStyle: TextStyle(fontSize: 18.0),
-              onTap: (){
+              onTap: () {
                 final RenderBox box = context.findRenderObject();
                 Share.share("${widget.scannedText}",
                     subject: "Scanned Results",
                     sharePositionOrigin:
-                    box.localToGlobal(Offset.zero) &
-                    box.size);
+                        box.localToGlobal(Offset.zero) & box.size);
               },
             )
           ],
